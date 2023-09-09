748fe6cb5954f87f003501559070ec74a3e608e6
Mart van der Zalm <martvanderzalm@gmail.com>
2023-09-09 21:27:29 +0200

    Making Unit tests

---
+++ test/lexer/LexerTest.java
@@ -0,0 +1,20 @@
+package test.lexer;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LexerTest
+{
+    @Before
+    public void setUp()
+    {
+        
+    }
+
+    @Test
+    public void testNextToken()
+    {
+ 
+    }
+}
