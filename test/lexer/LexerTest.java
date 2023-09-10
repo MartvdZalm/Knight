b83f0cbac8c5605305380d0e29363e5e1515933a
Mart van der Zalm <martvanderzalm@gmail.com>
2023-09-10 22:54:04 +0200

    Unit tests

--- test/lexer/LexerTest.java
+++ test/lexer/LexerTest.java
@@ -4,17 +4,23 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import src.lexer.*;
+
 public class LexerTest
 {
+    private Token token;
+    private Lexer lexer;
+
     @Before
     public void setUp()
     {
-        
+        lexer = new Lexer("example.knight");
+        token = lexer.nextToken();
     }
 
     @Test
-    public void testNextToken()
+    public void nextTokenTest()
     {
- 
+        assertEquals("Start object found", Tokens.CLASS, token.getToken());
     }
 }
