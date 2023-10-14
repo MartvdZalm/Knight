b3d589db4b876b9f470610ed1b88e6950215d9f5
Mart van der Zalm <martvanderzalm@gmail.com>
2023-10-14 12:37:44 +0200

    Documentation
    This will be removed and will later be added again but better.

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ast"},{"l":"lexer"},{"l":"parser"},{"l":"semantics"},{"l":"symbol"},{"l":"visitor"}];updateSearchResults();
