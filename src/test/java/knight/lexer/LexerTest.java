b09f0ee7b77a995ac254c6c32fcf0e459dc8e05b
Mart van der Zalm <martvanderzalm@gmail.com>
2024-01-06 23:59:10 +0100

    LexerTest

--- src/test/java/knight/lexer/LexerTest.java
+++ src/test/java/knight/lexer/LexerTest.java
@@ -22,25 +22,55 @@ public class LexerTest
 	}
 
     @Test
-    public void testTokenization()
+    public void tokenizationTest()
     {
-        assertToken(Tokens.INCLUDE, "include");
-        assertToken(Tokens.STRING, "print");
-        assertToken(Tokens.SEMICOLON, ";");
-
-
-
-        assertToken(Tokens.FUNCTION, "fn");
-        assertToken(Tokens.IDENTIFIER, "main");
-        assertToken(Tokens.LEFTPAREN, "(");
-        assertToken(Tokens.RIGHTPAREN, ")");
-        assertToken(Tokens.COLON, ":");
-        assertToken(Tokens.INTEGER, "int");
-        assertToken(Tokens.LEFTBRACE, "{");
-        assertToken(Tokens.RETURN, "ret");
-        assertToken(Tokens.INTEGER, "0");
-        assertToken(Tokens.SEMICOLON, ";");
-        assertToken(Tokens.RIGHTBRACE, "}");
+    	assertToken(Tokens.LEFTPAREN, "(");
+		assertToken(Tokens.RIGHTPAREN, ")");
+		assertToken(Tokens.LEFTBRACE, "{");
+		assertToken(Tokens.RIGHTBRACE, "}");
+		assertToken(Tokens.LEFTBRACKET, "[");
+		assertToken(Tokens.RIGHTBRACKET, "]");
+		assertToken(Tokens.SEMICOLON, ";");
+		assertToken(Tokens.COLON, ":");
+		assertToken(Tokens.COMMA, ",");
+		assertToken(Tokens.DOT, ".");
+		assertToken(Tokens.IDENTIFIER, "name");
+		assertToken(Tokens.INTEGER, "0");
+		assertToken(Tokens.STRING, "string");
+		assertToken(Tokens.BOOLEAN, "bool");
+		assertToken(Tokens.TRUE, "true");
+		assertToken(Tokens.FALSE, "false");
+		assertToken(Tokens.PUBLIC, "public");
+		assertToken(Tokens.PROTECTED, "protected");
+		assertToken(Tokens.PRIVATE, "private");
+		assertToken(Tokens.CLASS, "class");
+		assertToken(Tokens.INTERFACE, "inter");
+		assertToken(Tokens.NEW, "new");
+		assertToken(Tokens.INCLUDE, "include");
+		assertToken(Tokens.FUNCTION, "fn");
+		assertToken(Tokens.ENUMERATION, "enum");
+		assertToken(Tokens.EXTENDS, "ext");
+		assertToken(Tokens.IMPLEMENTS, "use");
+		assertToken(Tokens.IF, "if");
+		assertToken(Tokens.ELSE, "else");
+		assertToken(Tokens.WHILE, "while");
+		assertToken(Tokens.FOR, "for");
+		assertToken(Tokens.ASSIGN, "=");
+		assertToken(Tokens.EQUALS, "==");
+		assertToken(Tokens.AND, "and");
+		assertToken(Tokens.OR, "or");
+		assertToken(Tokens.LESSTHAN, "<");
+		assertToken(Tokens.LESSTHANOREQUAL, "<=");
+		assertToken(Tokens.GREATERTHAN, ">");
+		assertToken(Tokens.GREATERTHANOREQUAL, ">=");
+		assertToken(Tokens.PLUS, "+");
+		assertToken(Tokens.INCREMENT, "++");
+		assertToken(Tokens.MINUS, "-");
+		assertToken(Tokens.TIMES, "*");
+		assertToken(Tokens.DIV, "/");
+		assertToken(Tokens.MODULUS, "%");
+		assertToken(Tokens.RETURN, "ret");
+		assertToken(Tokens.VOID, "void");
     }
 
     private void assertToken(Tokens expectedToken, String input)
