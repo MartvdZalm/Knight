56ae4c2a3c8fb32c0f71b19758e15e23256adaca
Mart van der Zalm <martvanderzalm@gmail.com>
2024-01-07 23:24:57 +0100

    ParserTest

--- src/test/java/knight/parser/ParserTest.java
+++ src/test/java/knight/parser/ParserTest.java
@@ -9,59 +9,61 @@ import knight.ast.*;
 
 public class ParserTest
 {
-	Tree tree;
+    Program program;
 
 	public ParserTest()
 	{
 		try {
-			InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream("ParserTest.txt");
+			InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream("ParserTest.knight");
 			Reader reader = new InputStreamReader(ioStream);
 			BufferedReader br = new BufferedReader(reader);
 
 			Parser parser = new Parser(br);
-			this.tree = parser.parse();
+			Tree tree = parser.parse();
+            this.program = (Program) tree;
 		} catch(ParseException e) {
 			e.printStackTrace();
 		}
 	}
 
-	@Test
-	public void programTest()
-	{
-        Program program = (Program) this.tree;
-
-        // Test include list
+    @Test
+    public void includeTest()
+    {
         assertEquals(2, program.getIncludeListSize());
+
         assertEquals("print", program.getIncludeDeclAt(0).getId().toString());
         assertEquals("math", program.getIncludeDeclAt(1).getId().toString());
+    }
 
-        // Test variable list
+    @Test
+    public void classTest()
+    {
+        assertEquals(1, program.getClassListSize());
+
+        assertEquals("person", program.getClassDeclAt(0).getId().toString());
+    }
+
+    @Test
+    public void functionTest()
+    {
+        assertEquals(2, program.getFunctionListSize());
+
+        assertEquals("calculate", program.getFunctionDeclAt(0).getId().toString());
+        assertTrue(program.getFunctionDeclAt(0).getReturnType() instanceof IntType);
+
+        assertEquals("main", program.getFunctionDeclAt(1).getId().toString());
+        assertTrue(program.getFunctionDeclAt(1).getReturnType() instanceof IntType);
+    }
+
+    @Test
+    public void variableTest()
+    {
         assertEquals(2, program.getVariableListSize());
 
-        // Test variable 1
-        Variable var1 = program.getVariableDeclAt(0);
-        assertEquals("num1", var1.getId().toString());
-        assertTrue(var1 instanceof VariableInit);
+        assertEquals("num1", program.getVariableDeclAt(0).getId().toString());
+        assertTrue(program.getVariableDeclAt(0) instanceof VariableInit);
 
-        // Test variable 1 type
-        assertTrue(var1.getType() instanceof IntType);
-
-        // Test variable 1 expression
-        VariableInit varInit1 = (VariableInit) var1;
-        Expression expr1 = varInit1.getExpr();
-        assertTrue(expr1 instanceof IntLiteral);
-
-        // Test variable 1 value
-        IntLiteral intLit1 = (IntLiteral) expr1;
-        assertEquals(20, intLit1.getValue());
-
-        // Test variable 2
-        Variable var2 = program.getVariableDeclAt(1);
-        assertEquals("result", var2.getId().toString());
-        assertTrue(var2 instanceof Variable);
-
-        // Test variable 2 type
-        assertTrue(var2.getType() instanceof IntType);
-
-	}
+        assertEquals("result", program.getVariableDeclAt(1).getId().toString());
+        assertTrue(program.getVariableDeclAt(1) instanceof Variable);
+    }
 }
