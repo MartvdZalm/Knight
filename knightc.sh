c87a0aec0d35276e2785b4989656b5743a510f0e
MartvdZalm <martvanderzalm@gmail.com>
2024-02-10 18:54:15 +0100

    knightc

--- knightc.sh
+++ knightc.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-# Set the CLASSPATH relative to the script's location
 CLASSPATH="$SCRIPT_DIR/target/classes"
-
-# Specify the main class
 MAIN_CLASS=knight.Main
 
-# Run the Java program
 java -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
